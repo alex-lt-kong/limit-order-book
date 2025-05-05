d913f879c081268cf8f7a481c527f69dece0a55a
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-05-05 12:39:35 +0800

    initial commit

---
+++ cmake-build-release/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mamsds/repos/proofs-of-concept/cpp/12_orderbook
+# Build directory: /home/mamsds/repos/proofs-of-concept/cpp/12_orderbook/cmake-build-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
